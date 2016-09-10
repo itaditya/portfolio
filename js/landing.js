43875a6f743f80077d82bd640b466ebba990c2ae
adi <adityaa803@gmail.com>
2016-09-10 21:11:10 +0530

    imrovs

--- js/landing.js
+++ js/landing.js
@@ -2,8 +2,9 @@
 function loadMainPage(){
     document.querySelector(".page").classList.add("animated");
     document.querySelector(".page").classList.add("fadeOut");
-    document.location.pathname = "/index.html";
+    // document.location.pathname = "/index.html";
 }
 setTimeout(function(){
-  loadMainPage()
-},21000);
+  loadMainPage();
+  document.querySelector("..transparent-img").style.opacity = 0.2;
+},26000);
