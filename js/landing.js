7c4c2db8486f52e772ea17698b8cb5c4447cc7db
adi <adityaa803@gmail.com>
2016-09-10 23:39:43 +0530

    Removes

--- js/landing.js
+++ js/landing.js
@@ -2,7 +2,7 @@
 function loadMainPage(){
     document.querySelector(".page").classList.add("animated");
     document.querySelector(".page").classList.add("fadeOut");
-    // document.location.pathname = "/index.html";
+    document.location.pathname = "/index.html";
 }
 setTimeout(function(){
   loadMainPage();
